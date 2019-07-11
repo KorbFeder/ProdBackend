6999015a561a3a840063f1785dafb469e5bb9e84
justarandom@justarandom <justarandom@korbinian.federholzner@t-online.de>
2019-07-11 08:05:59 +0200

    added Database Connection

---
+++ config/dbConnection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+module.exports = () => {
+    return mysql.createPool({
+        host: 'producitvity_db',
+        user: 'admin',
+        password: 'admin', 
+        database: 'productivity_db',
+        connectionLimit: '10'
+    });
+}
--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const dbConnection = require('../config/dbConnection');
 
-router.get('/', function(req, res, next) {
-    res.send(true);
+const con = dbConnection();
+
+con.on('connection', (connection) => {
+    console.log('new connection is made: ' + connection.threadId);
+})
+
+router.get('/', (req, res, next) => {
+    con.query('SELECT * FROM todos', (error, result, fields) => {
+        if(error){
+            console.log('an error has occoured: ' + error);
+            res.status(500).send(error);
+        }else{
+            console.log('solution: ' + result)
+            res.status(200).send(result);
+        }
+    });
 });
 
 module.exports = router;
