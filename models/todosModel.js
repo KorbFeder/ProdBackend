8ad531cf454d00cff5c75676116a66ca066394a3
JustARand0m <korbinian.federholzner@t-online.de>
2019-07-12 21:37:56 +0200

    added comments and saveTodo function

--- models/todosModel.js
+++ models/todosModel.js
@@ -8,6 +8,10 @@ con.on('connection', (connection) => {
 })
 
 module.exports = {
+    /**
+     * This function returns all todos from the database, if an error occours the error will be found 
+     * in the rejected error. 
+     */
     getAllTodos: function() {
         return new Promise((resolve, reject) => {
             con.query('SELECT * FROM todos', (error, result, fields) => {
@@ -20,6 +24,12 @@ module.exports = {
         });
     },
 
+    /**
+     * This method returns the specific todo with the the given ID, if an error occours, the error will be 
+     * found in the rejected error.
+     * 
+     * @param {number} id 
+     */
     getTodo: function(id) {
         return new Promise((resolve, reject) => {
             con.query(`SELECT * FROM todos WHERE id = ${mysql.escape(id)}`, (error, result, fields) => {
@@ -32,7 +42,29 @@ module.exports = {
         });
     },
 
-    saveTodos: function() {
-
+    /**
+     * This method inserts a todo object into the database, if an error occours, the error will be found
+     * in the rejected error.
+     * The todo object should contain the following attributes:
+     * id: number
+     * isDone: boolean,
+     * todoMsg: string,
+     * importance: int,
+     * endDate, Date,
+     *  
+     * @param {object} todo 
+     */
+    saveTodo: function(todo) {
+        return new Promise((resolve, reject) => {
+            con.query(`INSERT INTO todos(id, isDone, todoMsg, importance, endDate) 
+                       VALUES ('${todo.id}', ${todo.isDone}, ${todo.todoMsg}, ${todo.importance}, ${todo.endDate})`, 
+                       (error, result) => {
+                if(error) {
+                    reject(error);
+                }else{
+                    resolve(result);
+                }
+            });
+        });
     }
 }
