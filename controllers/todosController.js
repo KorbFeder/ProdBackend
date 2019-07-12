a199b5c4b6568e409f9fbff781026e284124a8ba
JustARand0m <korbinian.federholzner@t-online.de>
2019-07-12 21:55:02 +0200

    added saveTodo to controller

--- controllers/todosController.js
+++ controllers/todosController.js
@@ -5,7 +5,7 @@ const todoModel = require('../models/todosModel');
 module.exports = {
     get: function(req, res, next) {
         //save id request somewhere
-        const id = req.id;
+        const id = req.params.id;
         if(!id) {
             todoModel.getAllTodos().then((result) => {
                 console.log(`result: ${result}`);
@@ -13,7 +13,7 @@ module.exports = {
             }).catch((error) => {
                 console.log(`an error has occoured: ${error}`);
                 res.status(405).send(error);
-            })
+            });
         }else{
             todoModel.getTodo(id).then((result) => {
                 console.log(`result: ${result}`);
@@ -21,10 +21,22 @@ module.exports = {
             }).catch((error) => {
                 console.log(`an error has occoured: ${error}`);
                 res.status(405).send(error);
-            })
+            });
         }
     },
-    put: function(req, res, next) {
-        
+    post: function(req, res, next) {
+        const todo = req.body;
+        if(todo) {
+            todoModel.saveTodo(todo).then((result) => {
+                console.log(`result: ${result}`);
+                res.status(200).send(result);
+            }).catch((error) => {
+                console.log(`an error has occoured ${error}`);
+                res.status(405).send(error);
+            });
+        }else{
+            console.log("request body was empty");
+            res.status(400).send("request body was empty")
+        }
     }
 }
